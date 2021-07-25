343023261f6e6765dc2c11f8c3b4862dd6fd1311
Robert <robert@pfm.team>
2021-07-25 18:36:43 +0300

    Work on transfers filter

--- deploy.sh
+++ deploy.sh
@@ -17,9 +17,9 @@ git add -A
 git commit -m 'deploy'
 
 # если вы публикуете по адресу https://<USERNAME>.github.io
- git push -f git@github.com:rkorzhoff/rkorzhoff.github.io.git master
+# git push -f git@github.com:rkorzhoff/rkorzhoff.github.io.git master
 
-# если вы публикуете по адресу https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+# если вы публикуете по адресу https://<USERNAME>.github.io/aviasales-widget
+ git push -f git@github.com:rkorzhoff/aviasales-widget.git master:gh-pages
 
 cd -
